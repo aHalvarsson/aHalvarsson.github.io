a24a4b54880a1c7f377957825ba72f48a493216a
aHalvarsson <anders.halvarsson@gmail.com>
2023-04-14 11:40:29 +0200

    testing another approach to the portfolio part

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,15 @@
 
 const portImg = document.querySelectorAll('.portfolioDiv');
 const exitBox = document.getElementById('clickToExitBox');
-const portWindow = document.getElementById('portfolioItems');
+const portWindow = document.getElementById('portfolio');
 const pItemArt = document.querySelectorAll('.pItemClass');
-const mainD = document.getElementById('mainDiv');
 const sButtons = document.querySelectorAll('li');
 
-let mainDivPos;
-let clickLeft = true;
 let portWindowOpen = false;
 let portWindowFinish = true;
 let sameIndex;
 let portItem;
-let xPosition;
-let xPosDVW;
+
 
 const searchForIndex = term => [...portImg].indexOf(term);
 
@@ -37,62 +33,34 @@ const portImgClick = event => {
         exitBox.style.display = 'block';
         exitBox.style.height = document.body.offsetHeight + 'px';
         exitBox.removeEventListener('click', abortPortfolio);
-        
 
         portItem = event.currentTarget;
-        portItem.removeEventListener('mouseout', portImgOut);
 
+        portItem.removeEventListener('mouseout', portImgOut);
         let img = portItem.querySelector('img');
         img.style.filter = 'grayscale(0%)';
         portItem.style.width = '8dvw';
 
-        const rect = portItem.getBoundingClientRect();
-        xPosition = rect.left + window.pageXOffset;
-
-        mainDivPos = mainD.getBoundingClientRect().left + window.pageXOffset;
-
-        let viewWidth = document.querySelector('html').clientWidth;
-        let viewHeight = document.querySelector('html').clientHeight;
-        xPosDVW = (xPosition/viewWidth) * 100;
         sameIndex = searchForIndex(portItem);
-
-        if(xPosition < (viewWidth/2)) {
-            pItemArt[sameIndex].style.left = xPosDVW - 18 + 'dvw';
-            portWindow.style.left = xPosDVW + 5.5 + 'dvw';
-            clickLeft = true;
-        } else {
-            pItemArt[sameIndex].style.left = xPosDVW - 18 + 'dvw';
-            portWindow.style.left = 'auto';
-            portWindow.style.right = (100 - xPosDVW) -5.5 + 'dvw';
-            clickLeft = false;
-        }
-        pItemArt[sameIndex].style.display = 'block';
-        let pHeight = pItemArt[sameIndex].clientHeight;
-        window.alert(pHeight);
-        if(pHeight < 704) pHeight = 44;
-        else pHeight = pHeight/16 + 8;
-        window.alert(pHeight);
-        portWindow.style.minHeight = pHeight + 'rem';
-        portItem.style.zIndex = '15';
         portWindow.style.display = 'block';
+        pItemArt[sameIndex].style.display = 'block';
+
+        let pHeight = pItemArt[sameIndex].offsetHeight;
+
+        if(pHeight < 704) pHeight = 48;
+        else pHeight = (pHeight/16) + 5.5;
         
-   
+        pItemArt[sameIndex].style.opacity = '1';
+        pItemArt[sameIndex].style.marginTop = 4 + 'rem';
+        pItemArt[sameIndex].style.transition = 'opacity 1.5s, margin 1s';
+        
+        portWindow.style.opacity = '0.9';
+        portWindow.style.height = pHeight + 'rem';
+        portWindow.style.transition = 'height 1s, opacity 1.5s';
+    
+        exitBox.style.opacity = '1';      
 
-        setTimeout(() => {
-
-            portWindow.style.opacity = '0.9';
-            exitBox.style.opacity = '1';
-            if(clickLeft) {
-                portWindow.style.width = 80 - 5.75 - xPosDVW + 'dvw';
-            } else {
-                portWindow.style.width = 80 - (100 - xPosDVW) + 5.5 + 'dvw';
-            }
-            portWindow.style.transition = 'width 2s, opacity 1s';
-            
-            pItemArt[sameIndex].style.left = (mainDivPos/viewWidth) * 100 + 'dvw';
-            pItemArt[sameIndex].style.opacity = '1';
-            pItemArt[sameIndex].style.transition = 'opacity 4s, left 2s';
-        }, 0);
+        portItem.style.zIndex = '15';
 
         setTimeout(() => {
             exitBox.addEventListener('click', abortPortfolio);
@@ -108,24 +76,25 @@ const abortPortfolio = () => {
     portWindowFinish = false;
     
     exitBox.style.opacity = '0';
-    
-    pItemArt[sameIndex].style.opacity = '0';
-    pItemArt[sameIndex].style.transition = 'opacity 1s';
-
-    portWindow.style.width = '2dvw';
+    portWindow.style.height = '0';
     portWindow.style.opacity = '0';
-    portWindow.style.transition = 'width 1s, opacity 1s';
+    portWindow.style.transition = 'height 1s, opacity 1s';
+    
+    pItemArt[sameIndex].style.opacity = '1';
+    pItemArt[sameIndex].style.marginTop = -10 + 'rem';
+    pItemArt[sameIndex].style.transition = 'opacity 1.5s, margin 1s';
 
     portItem.addEventListener('mouseout', portImgOut);
-
     portItem.querySelector('img').style.filter = 'grayscale(100%)';
     portItem.style.width = '5.75dvw';
     
     setTimeout(() => {
-        portWindow.style.display = 'none';
         pItemArt[sameIndex].style.display = 'none';
-        portItem.style.zIndex = 'auto';
+        portWindow.style.display = 'none';
         exitBox.style.display = 'none';
+        
+        portItem.style.zIndex = 'auto';
+        
         portWindowOpen = false;
         portWindowFinish = true;
     }, 1000)
